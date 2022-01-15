3c1cb6d4e8c84889592c1aecb5aa61e980559e5f
leo4545525 <95300026+leo4545525@users.noreply.github.com>
2022-01-15 16:14:01 +0800

    2022/1/15 pm-04/13

--- Gamers Basement/index.js
+++ Gamers Basement/index.js
@@ -6,6 +6,10 @@ let csgoplayer01, csgoplayer02, csgoplayer03, csgoplayer04, csgoplayer05;
 let valorantplayer01, valorantplayer02, valorantplayer03, valorantplayer04, valorantplayer05;
 let searchResults;
 let apex_NRG, apex_TSM, apex_TeamLiquid, apex_Cloud9, apex_G2;
+//////////////////////////////////////////////////////////
+let csgo_NatusVincere, csgo_G2Esports, csgo_Astralis, csgo_TeamVitality, csgo_TeamLiquid;
+let valorant_Acend, valorant_Fnatic, valorant_Gambit, valorant_SENTINELS, valorant_TeamLiquid;
+
 
 function apex_random_select() {
     for(var i=0; i<5; i++) {
@@ -21,6 +25,34 @@ function csgo_random_select() {
 }
 
 function valorant_random_select() {
+
+}
+
+function csgoBtnClick() {
+    csgo_NatusVincere = document.getElementById("csgo_NatusVincere");
+    csgo_NatusVincere.addEventListener("click", function () { document.getElementById("csgoTeamBtn").innerHTML = "Natus Vincere" }, false);
+    csgo_G2Esports = document.getElementById("csgo_G2Esports");
+    csgo_G2Esports.addEventListener("click", function () { document.getElementById("csgoTeamBtn").innerHTML = "G2 Esports" }, false);
+    csgo_Astralis = document.getElementById("csgo_Astralis");
+    csgo_Astralis.addEventListener("click", function () { document.getElementById("csgoTeamBtn").innerHTML = "Astralis" }, false);
+    csgo_TeamVitality = document.getElementById("csgo_TeamVitality");
+    csgo_TeamVitality.addEventListener("click", function () { document.getElementById("csgoTeamBtn").innerHTML = "Team Vitality" }, false);
+    csgo_TeamLiquid = document.getElementById("csgo_TeamLiquid");
+    csgo_TeamLiquid.addEventListener("click", function () { document.getElementById("csgoTeamBtn").innerHTML = "Team Liquid" }, false);
+}
+
+function valorantBtnClick(){
+    valorant_Acend = document.getElementById("valorant_Acend");
+    valorant_Acend.addEventListener("click", function () { document.getElementById("valorantTeamBtn").innerHTML = "Acend" }, false);
+    valorant_Fnatic = document.getElementById("valorant_Fnatic");
+    valorant_Fnatic.addEventListener("click", function () { document.getElementById("valorantTeamBtn").innerHTML = "Fnatic" }, false);
+    valorant_Gambit = document.getElementById("valorant_Gambit");
+    valorant_Gambit.addEventListener("click", function () { document.getElementById("valorantTeamBtn").innerHTML = "Gambit" }, false);
+    valorant_SENTINELS = document.getElementById("valorant_SENTINELS");
+    valorant_SENTINELS.addEventListener("click", function () { document.getElementById("valorantTeamBtn").innerHTML = "SENTINELS" }, false);
+    valorant_TeamLiquid = document.getElementById("valorant_TeamLiquid");
+    valorant_TeamLiquid.addEventListener("click", function () { document.getElementById("valorantTeamBtn").innerHTML = "Team Liquid" }, false);
+
 
 }
 
@@ -42,11 +74,12 @@ function search() {
 }
 
 function start(){
+    apexBtnClick();
+    csgoBtnClick();
+    valorantBtnClick();
     apex_random_select();
     csgo_random_select();
     valorant_random_select();
-    apexBtnClick();
-
 }
 
 window.addEventListener("load", start, false)

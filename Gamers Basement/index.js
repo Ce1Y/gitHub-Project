4a9ed91aecc74ca6f5b9ace159de3f506c5e3127
Ce1Y <sycn02430@gmail.com>
2022-01-15 15:35:03 +0800

    new

--- Gamers Basement/index.js
+++ Gamers Basement/index.js
@@ -5,7 +5,7 @@ let apexplayer01, apexplayer02, apexplayer03, apexplayer04, apexplayer05;
 let csgoplayer01, csgoplayer02, csgoplayer03, csgoplayer04, csgoplayer05;
 let valorantplayer01, valorantplayer02, valorantplayer03, valorantplayer04, valorantplayer05;
 let searchResults;
-let apexNRG, apexTSM, apexTeamLiquid, apexCloud9, apexG2;
+let apex_NRG, apex_TSM, apex_TeamLiquid, apex_Cloud9, apex_G2;
 
 function apex_random_select() {
     for(var i=0; i<5; i++) {
@@ -13,8 +13,7 @@ function apex_random_select() {
     }
     apexplayer01 = document.getElementById("apexplayer01");
     apexplayer01.src = 'srcpack/Apex_Gamers/Aceu.png';
-    // apexNRG = document.getElementById("apexNRG");
-    // apexNRG.addEventListener("click", function(){document.getElementById("apexTeamBtn").innerHTML = "NRG"}, false);
+    
 }
 
 function csgo_random_select() {
@@ -26,16 +25,16 @@ function valorant_random_select() {
 }
 
 function apexBtnClick() {
-    apexNRG = document.getElementById("apex_NRG");
-    apexNRG.addEventListener("click", function(){document.getElementById("apexTeamBtn").innerHTML = "NRG"}, false);
-    apexTSM = document.getElementById("apex_TSM");
-    apexTSM.addEventListener("click", function(){document.getElementById("apexTeamBtn").innerHTML = "Team SoloMid"}, false);
-    apexTeamLiquid = document.getElementById("apex_TeamLiquid");
-    apexTeamLiquid.addEventListener("click", function(){document.getElementById("apexTeamBtn").innerHTML = "Team Liquid"}, false);
-    apexCloud9 = document.getElementById("apex_Cloud9");
-    apexCloud9.addEventListener("click", function(){document.getElementById("apexTeamBtn").innerHTML = "Cloud9"}, false);
-    apexG2 = document.getElementById("apex_G2");
-    apexG2.addEventListener("click", function(){document.getElementById("apexTeamBtn").innerHTML = "G2 Esports"}, false);
+    apex_NRG = document.getElementById("apex_NRG");
+    apex_NRG.addEventListener("click", function(){document.getElementById("apexTeamBtn").innerHTML = "NRG"}, false);
+    apex_TSM = document.getElementById("apex_TSM");
+    apex_TSM.addEventListener("click", function(){document.getElementById("apexTeamBtn").innerHTML = "Team SoloMid"}, false);
+    apex_TeamLiquid = document.getElementById("apex_TeamLiquid");
+    apex_TeamLiquid.addEventListener("click", function(){document.getElementById("apexTeamBtn").innerHTML = "Team Liquid"}, false);
+    apex_Cloud9 = document.getElementById("apex_Cloud9");
+    apex_Cloud9.addEventListener("click", function(){document.getElementById("apexTeamBtn").innerHTML = "Cloud9"}, false);
+    apex_G2 = document.getElementById("apex_G2");
+    apex_G2.addEventListener("click", function(){document.getElementById("apexTeamBtn").innerHTML = "G2 Esports"}, false);
 }
 
 function search() {
